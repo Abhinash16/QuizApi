2be5480cc3a913b5853651fd565c189195bb0ab0
Abhinash PC <avinash16595@gmail.com>
2019-09-12 20:22:33 +0530

    commit

---
+++ frontend/quiz-frontend/src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="show-question">
+    <h1></h1>
+</div>
+</template>
+
+<script>   
+export default {
+  name: 'home',
+  props: {
+  },
+  created(){
+    this.$http.get('localhost:8080/api/questions/').then(function(data){
+      console.log(data)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
